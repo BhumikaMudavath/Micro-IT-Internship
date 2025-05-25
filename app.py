c648e27714ac8b6a3355f21ff69132dc782ffc2c
BhumikaMudavath <bhoomikamudavath09@gmail.com>
2025-05-25 15:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,6 @@ def index():
                          emotion=emotion, confidence=confidence, scores=scores)
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
